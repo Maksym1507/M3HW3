442487d1246161c40a3bb57c7a5dd98f0c07df29
Max <maksym.illarionov@nure.ua>
2022-10-17 02:48:42 +0300

    Was implemented task to to consolidate the knowledge of the delegates

---
+++ M3HW3/Class2.cs
@@ -0,0 +1,19 @@
+﻿namespace M3HW3
+{
+    public class Class2
+    {
+        private int _result;
+
+        public Predicate<int> Calc(Func<int, int, int> multiplyHandler, int firstNumber, int secondNumber)
+        {
+            _result = multiplyHandler(firstNumber, secondNumber);
+            return Result;
+        }
+
+        public bool Result(int number)
+        {
+            _result %= number;
+            return _result % 2 == 0;
+        }
+    }
+}
---
+++ M3HW3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace M3HW3
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var class1 = new Class1
+            {
+                ActionHandler = new Program().Show
+            };
+
+            var class2 = new Class2();
+            class2.Calc(class1.Multiply, 10, 15);
+
+            class1.ActionHandler(class2.Result(11));
+        }
+
+        private void Show(bool result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
